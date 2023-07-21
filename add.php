7a7c25532158da194bda7472df0556ebd6914461
Daniel <cawasdn@gmail.com>
2023-07-21 14:50:12 +0800

    add.php changes

--- add.php
+++ add.php
@@ -16,12 +16,14 @@ if (isset($_POST['submit'])) {
     }
 
     // check title
+
+
     if (empty($_POST['title'])) {
-        $errors['title'] = 'A title is required <br />';
+        $errors['title'] = 'A title is required';
     } else {
         $title = $_POST['title'];
-        if (!preg_match('/^[a-zA-Z\s]/', $title)) {
-            $errors['title'] = 'Title must be a letters and spaces only';
+        if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
+            $errors['title'] = 'Title must be letters and spaces only';
         }
     }
 
@@ -48,17 +50,17 @@ if (isset($_POST['submit'])) {
 <section class="container grey-text">
     <h4 class="center">Add a Pizza</h4>
     <form action="add.php" method="POST" class="white">
-        <label for="">Your Email</label>
+        <label>Your Email</label>
         <input type="text" name="email" value="<?php echo htmlspecialchars($email); ?>">
         <div class="red-text"><?php echo $errors['email']; ?></div>
-        <label for="">Pizza Title</label>
+        <label>Pizza Title</label>
         <input type="text" name="title" value="<?php echo htmlspecialchars($title); ?>">
-        <div class=" red-text"><?php echo $errors['title']; ?></div>
-        <label for="">Ingredients (comma separated):</label>
+        <div class="red-text"><?php echo $errors['title']; ?></div>
+        <label>Ingredients (comma separated)</label>
         <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients); ?>">
         <div class="red-text"><?php echo $errors['ingredients']; ?></div>
         <div class="center">
-            <input type="submit" name="submit" value="submit" class="btn brand z-depthj-0">
+            <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
         </div>
     </form>
 </section>
