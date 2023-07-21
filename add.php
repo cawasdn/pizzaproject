a8fcc698c2c0789d986828fb87d72a8bbc549129
Daniel <cawasdn@gmail.com>
2023-07-21 13:10:01 +0800

    add.php fix

--- add.php
+++ add.php
@@ -1,10 +1,39 @@
 <?php
 
 if (isset($_POST['submit'])) {
-    echo $_POST['email'];
-    echo $_POST['title'];
-    echo $_POST['ingredients'];
-}
+
+    // check email
+    if (empty($_POST['email'])) {
+        echo 'An email is required <br />';
+    } else {
+        $email = $_POST['email'];
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo 'email must be a valid email address';
+        }
+    }
+
+    // check title
+    if (empty($_POST['title'])) {
+        echo 'A title is required <br />';
+    } else {
+        $title = $_POST['title'];
+        if (!preg_match('/^[a-zA-Z\s]/', $title)) {
+            echo 'Title must be a letters and spaces only';
+        }
+    }
+
+    //check ingredients
+
+    if (empty($_POST['ingredients'])) {
+        echo 'At least one ingredient is required <br />';
+    } else {
+        $ingredients = $_POST['ingredients'];
+        if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
+            echo 'Ingredients  must be a comma separated list';
+        }
+    }
+} // end post check
+
 
 ?>
 
