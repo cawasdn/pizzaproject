890cc6ab97f2a46bd0c13e2043feef62beb2ca92
Daniel <cawasdn@gmail.com>
2023-07-23 00:25:10 +0800

    PizzaProject

--- templates/header.php
+++ templates/header.php
@@ -5,17 +5,24 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <style type="text/css">
         .brand {
-            background: #cbb09c !important;
+            background: #5cb85c !important;
         }
 
         .brand-text {
-            color: #cbb09c !important;
+            color: #5cb85c !important;
         }
 
         form {
             max-width: 460px;
             margin: 20px auto;
             padding: 20px;
+        }
+        .pizza{
+            width: 100px;
+            margin:40px auto -30px;
+            display:block;
+            position: relative;
+            top: -30px;
         }
     </style>
 
