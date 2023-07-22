35fb9b29ce8f8ecedbb548693f05791d2f1888e6
Daniel <cawasdn@gmail.com>
2023-07-23 00:11:15 +0800

    add delete form

--- details.php
+++ details.php
@@ -1,9 +1,24 @@
 <?php
     include('config/db_connect.php');
 
+    if(isset($_POST['delete'])){
+        $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+        $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+        if(mysqli_query($conn,$sql)){
+            // success
+            header('Location: index.php');
+        } else {
+            // failure
+            echo 'query error:' . mysqli_error($conn);
+        }
+    }
+
     //check GET request id param
     if(isset($_GET['id'])){
         $id = mysqli_real_escape_string($conn, $_GET['id']);
+
         // make sql
         $sql = "SELECT * FROM pizzas WHERE id = $id";
 
@@ -33,6 +48,11 @@
         <p><?php echo date($pizza['created_at']); ?></p>
         <h5>Ingredients:</h5>
         <p><?php echo htmlspecialchars($pizza['ingredients']) ?></p>
+        <!-- Delete form -->
+        <form action="details.php" method="POST">
+            <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id'] ?>">
+            <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+        </form>
     <?php else: ?>     
         <h5>No such pizza exist</h5>
     <?php endif ?>
